85769de48640e2cea8cee7464453d8d339d80eb6
Arman Khan <sparmankhan@gmail.com>
2023-06-22 22:51:27 +0600

    style update

--- pages/index.js
+++ pages/index.js
@@ -10,26 +10,25 @@ const index = () => {
   const viewUrl = downnloadUrl.split("/")[3];
 
   return (
-    <Layout
-      title={"Download Youtube Videos and Audio"}
-      className="flex min-h-screen flex-col items-center p-24"
-    >
-      <div className="flex items-center">
-        <input
-          onChange={(e) => setDownloadUrl(e.target.value)}
-          placeholder="https://www.youtube.com/watch?v=vuNGR9jJspE"
-          className="input input-bordered"
-          type="url"
-        />
+    <Layout title={"Download Youtube Videos and Audio"}>
+      <div className="flex min-h-screen flex-col items-center p-24">
+        <div className="flex items-center">
+          <input
+            onChange={(e) => setDownloadUrl(e.target.value)}
+            placeholder="https://www.youtube.com/watch?v=vuNGR9jJspE"
+            className="input input-bordered"
+            type="url"
+          />
 
-        <button
-          onClick={() => {
-            return router.push(`/${viewUrl}`);
-          }}
-          className="btn btn-warning"
-        >
-          Download
-        </button>
+          <button
+            onClick={() => {
+              return router.push(`/${viewUrl}`);
+            }}
+            className="btn btn-warning"
+          >
+            Download
+          </button>
+        </div>
       </div>
     </Layout>
   );
